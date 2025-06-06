838bde94315f8bdeb28fcbb8f921048b74023402
sum1sum <agullana.samuel21@gmail.com>
2025-06-06 22:03:41 +0800

    Enhanced App.jsx and tweaked Index.css

--- MovieStar/src/App.jsx
+++ MovieStar/src/App.jsx
@@ -3,33 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Card = ({ title }) =>{
+    return (
+        <div style={{
+            border: '1px solid #4b5362',
+            padding: '20px',
+            backgroundColor: '#31363f',
+            borderRadius: '10px',
+            minHeight: '100px',
+        }}>
+            <h2>{title}</h2>
+        </div>
+    )
 }
-
+const App = () => {
+    return (
+        <div className="card-container">
+            <Card title="Star Wars" rating={5} isCool={true}/>
+            <Card title="Avatar"/>
+            <Card title="The Lion King"/>
+        </div>
+    )
+}
 export default App
